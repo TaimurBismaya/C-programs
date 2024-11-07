f7104135c05db6e9959425c6e4db8f38fc06ac61
TaimurBismaya <bismayapoolakkanny@gmail.com>
2024-11-07 12:48:35 +0530

    Add files via upload

---
+++ average-of-3-numbers/main.c
@@ -0,0 +1,18 @@
+
+#include <stdio.h>
+
+int main()
+{
+    int a,b,c,d;
+    printf("enter the first number:");
+    scanf("%d",&a);
+    printf("enter the second number:");
+    scanf("%d",&b);
+    printf("enter the third number:");
+    scanf("%d",&c);
+    d=(a+b+c)/3;
+    printf("average %d",d);
+    
+
+    return 0;
+}
